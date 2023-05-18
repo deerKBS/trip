2bdfb044230c2079113e5470c2fca9a808c67027
deerKBS <deer980119@gmail.com>
2023-05-18 17:59:09 +0900

    change

--- trip/src/Layout/TopNavBar.vue
+++ trip/src/Layout/TopNavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>TopNavBar</h1>
-    <router-link to="/auth/login" class="btn">로그인</router-link>
-    <router-link to="/auth/register" class="btn">회원가입</router-link>
+    <router-link to="/login" class="btn">로그인</router-link>
+    <router-link to="/register" class="btn">회원가입</router-link>
   </div>
 </template>
 
