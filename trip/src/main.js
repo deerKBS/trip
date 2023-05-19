081c084cb02bb698559b4ad659a6514298066cd5
jair9002 <cwoh9002@naver.com>
2023-05-19 11:32:15 +0900

    calendar

--- trip/src/main.js
+++ trip/src/main.js
@@ -8,16 +8,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 // import ans design for side bar
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 Vue.use(Antd);
 
 // tailwind
 import "./index.css";
 
+// vuetify
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+Vue.use(Vuetify);
+
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify: new Vuetify(),
   render: (h) => h(App),
   router,
   store,
---
+++ trip/src/view/pages/CalendarPage2.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="pa-5">
+    <v-row>
+      <v-col cols="12" md="6" class="mb-4">
+        <v-row>
+          <v-col cols="6">
+            <v-menu ref="dateOpen" v-model="dateOpen" :close-on-content-click="false" :return-value.sync="start" offset-y>
+              <template v-slot:activator="{ on }">
+                <v-text-field v-model="start" dark label="Start Date" prepend-icon="mdi-calendar" dense readonly outlined hide-details v-on="on"></v-text-field>
+              </template>
+
+              <v-date-picker dark v-model="start" no-title>
+                <v-spacer />
+                <v-btn text dark color="primary" @click="dateOpen = false">Cancel</v-btn>
+                <v-btn text dark color="primary" @click="$refs.dateOpen.save(start)">OK</v-btn>
+              </v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col cols="6">
+            <v-select v-model="type" :items="typeOptions" label="Type" class="my-auto" dark hide-details outlined dense></v-select>
+          </v-col>
+        </v-row>
+        <v-sheet height="500">
+          <v-calendar ref="calendar" :start="start" :type="type" dark></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dateOpen: false,
+      start: "",
+      type: "month",
+      typeOptions: [
+        { text: "Day", value: "day" },
+        { text: "Week", value: "week" },
+        { text: "Month", value: "month" },
+      ],
+    };
+  },
+};
+</script>
