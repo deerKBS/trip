f80581dd0a9afa68165028c7958c789fe1e8e2da
jair9002 <cwoh9002@naver.com>
2023-05-20 16:41:14 +0900

    login localstorage

--- trip/src/Layout/TopNavBar.vue
+++ trip/src/Layout/TopNavBar.vue
@@ -2,7 +2,7 @@
   <a-layout-header class="header bg-sky-200 shadow-current">
     <router-link v-show="!$store.state.login.isLogin" to="/login" class="btn">로그인</router-link>
     <router-link v-show="!$store.state.login.isLogin" to="/register" class="btn">회원가입</router-link>
-    <router-link v-show="$store.state.login.isLogin" to="/login" class="btn">로그아웃</router-link>
+    <a @click="logout" v-show="$store.state.login.isLogin" to="/login" class="btn">로그아웃</a>
     <router-link v-show="$store.state.login.isLogin" to="/userProfile" class="btn">회원정보</router-link>
   </a-layout-header>
 </template>
@@ -10,5 +10,14 @@
 <script>
 export default {
   name: "TopNavBar",
+
+  methods: {
+    logout() {
+      
+      localStorage.setItem("isLogin","false");
+      console.log("로그아웃 "+localStorage.getItem("isLogin"));
+      this.$store.commit("SET_LOGOUT");
+    }
+  }
 };
 </script>
