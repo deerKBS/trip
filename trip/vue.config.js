0c8f4c06d7420924cf9556dd0440b6d200eafd5a
deerKBS <deer980119@gmail.com>
2023-05-26 05:44:34 +0900

    export

--- trip/vue.config.js
+++ trip/vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// module.exports = {
+//   outputDir :"C:/SSAFY/stsWorkspace/Trip/src/main/resources/static"
+// }
