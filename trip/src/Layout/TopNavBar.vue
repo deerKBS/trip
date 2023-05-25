4f70bbf9686a9fdac9988b42f524c3fda3b21848
deerKBS <deer980119@gmail.com>
2023-05-25 17:39:19 +0900

    revise

--- trip/src/Layout/TopNavBar.vue
+++ trip/src/Layout/TopNavBar.vue
@@ -2,14 +2,14 @@
   <a-layout-header class="header bg-sky-200 shadow-current" style="width: 100vw">
     <div class="flex items-center justify-between">
       <div class="flex items-center">
-        <img src="../assets/logo.png" class="mr-2" style="height: 64px" />
-        <div>
-          <router-link v-show="!$store.state.login.isLogin" to="/login" class="btn">로그인</router-link>
-          <router-link v-show="!$store.state.login.isLogin" to="/register" class="btn">회원가입</router-link>
-        </div>
+        <router-link to="/main">
+          <img src="../assets/logo.png" class="mr-2" style="height: 64px" />
+        </router-link>
       </div>
       <div class="flex items-center justify-end">
-        <router-link @click="logout" v-show="$store.state.login.isLogin" to="/" class="btn">로그아웃</router-link>
+        <router-link v-show="!$store.state.login.isLogin" to="/login" class="btn">로그인</router-link>
+        <router-link v-show="!$store.state.login.isLogin" to="/register" class="btn">회원가입</router-link>
+        <a @click="logout" v-show="$store.state.login.isLogin" to="/" class="btn">로그아웃</a>
         <router-link v-show="$store.state.login.isLogin" to="/userProfile" class="btn">회원정보</router-link>
       </div>
     </div>
--- trip/src/view/pages/MainPage.vue
+++ trip/src/view/pages/MainPage.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>메인페이지</div>
+  <div class="w-full h-full p-4 d-flex justify-content-center">
+    <div class="bg-sky-100 container-fluid p-5 shadow-lg">
+      <!-- 캐러셀 -->
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="width: 70%">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="../../assets/img/1.jpg" class="d-block w-100" />
+          </div>
+          <div class="carousel-item">
+            <img src="../../assets/img/2.jpg" class="d-block w-100" />
+          </div>
+          <div class="carousel-item">
+            <img src="../../assets/img/3.jpg" class="d-block w-100" />
+          </div>
+          <div class="carousel-item">
+            <img src="../../assets/img/4.jpg" class="d-block w-100" />
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
